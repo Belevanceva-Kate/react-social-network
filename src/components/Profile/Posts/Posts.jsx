bea06e943bd825fc649e2bd1dca47b5de234a3f6
Kate <belekate2711@gmail.com>
2021-03-06 17:25:18 +0200

    comparing shouldComponentUpdate, PureComponent, memo

--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Post from './Post/Post';
 import PostForm from './PostForm/PostForm';
 import cls from './Posts.module.css';
@@ -25,4 +26,47 @@ const Posts = (props) => {
 	);
 }
 
-export default Posts;
+export default React.memo(Posts);
+
+
+
+
+/*
+// import { Component } from 'react';
+import { PureComponent } from 'react';
+import Post from './Post/Post';
+import PostForm from './PostForm/PostForm';
+import cls from './Posts.module.css';
+
+class Posts extends PureComponent {
+// class Posts extends Component {
+
+	// shouldComponentUpdate(nextProps, nextState) {
+	// 	return nextProps !== this.props || nextState !== this.state;
+	// }
+
+	render() {
+
+		let postElements = this.props.posts.map((el, i) => {
+			return <Post message={el.message} likesCount={el.likesCount} key={i.toString()}/>
+		});
+
+		const onFormSubmit = (values) => {
+			this.props.addPost(values.post);
+		}
+
+		return (
+			<div className={cls.postsBlock}>
+				<h3>My posts</h3>
+				<PostForm onSubmit={onFormSubmit}/>
+				<div className={cls.posts}>
+					<ul className={cls.list}>
+						{postElements}
+					</ul>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Posts;*/
